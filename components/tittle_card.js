c97b626096b051a5cb65aa2d5e81ff877dbbb046
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-04-17 20:43:54 +0530

    tittle card content added

--- components/tittle_card.js
+++ components/tittle_card.js
@@ -1,7 +1,21 @@
 const TittleCard = ({children}) => {
     return (
-        <div className="tittle-card">
-            {children}
+        <div className="tittle-card flex items-center  flex-col">
+            <p className="m-4 text-center">
+                <span className="text-5xl font-extrabold">Wellcome!! </span>
+                <span className="text-3xl block">This is anonymous contributions application.</span>
+            </p>
+            <p className="m-2 mt-0 text-center">
+                <span className="text-xl">
+                    Here you can create a fund and raise money while staying anonymous.
+                    We use zero knowledge proof to verify your identity while keeping it hidden even from us.
+                </span>
+            </p>
+            <p className="m-2 text-center">
+                <span className="text-xl">Click the button bellow to create new fund.</span>
+                <span className="text-xl block">You can also donate to existing funds by clicking on donate button in respective card.</span>
+            </p>
+            <a className="mt-auto"><button className="button font-medium">Create New Fund!!</button></a>
         </div>
         )
     }
