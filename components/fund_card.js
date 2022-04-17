c98f9f25803bcf5e5db840bcc1d81fd64949d421
Nisarg Chaudhari <nisarg5601@gmail.com>
2022-04-17 16:27:10 +0530

    Fund card (#2)
    * 404 page added
    * fund card layout added
    
    Co-authored-by: Nisarg Chaudhari <nisrg5601@gmail.com>

--- components/fund_card.js
+++ components/fund_card.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const FundCard = ({children}) => {
     return (
-        <div className="fund-card flex items-center justify-end">
-            {children}
+        <div className="fund-card flex items-center justify-center flex-col">
+            <p className="mt-2 text-lg font-medium">Tittle of the Fund</p>
+            <Link href="funds/1"><a><button className="button">Donate!!</button></a></Link>
         </div>
         )
     }
