65e12c643dd38bfa77cdbabd41080ad2270a850e
Nisarg Chaudhari <nisarg5601@gmail.com>
2022-04-26 18:26:57 +0530

    Forms working (#9)

--- pages/funds/[id].js
+++ pages/funds/[id].js
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import Donation from "../../components/donation";
 import FullpageCard from "../../components/fullpage_card";
+import React from 'react';
+import Router from 'next/router'
 
-export default function FundDetails({ fund }) {
+export default function FundDetails({ fund, fund_id }) {
+    
+    let amountRef = React.createRef();
+    let panRef = React.createRef();
+
     return (
         <FullpageCard>
             <div className="flex flex-col">
@@ -17,9 +23,16 @@ export default function FundDetails({ fund }) {
                 </p>
 
                 <div className="m-responsive w-fit self-center flex flex-col items-center bg-gray-100 bg-opacity-20 rounded-md shadow-md">
-                    <input type="number" placeholder="Amount" className="input flex-1"></input>
-                    <input type="password" placeholder="PAN number" className="input flex-1"></input>
-                    <button className="button w-fit"> <a> Donate </a></button>
+                    <input ref={amountRef} type="number" min="0" placeholder="Amount" className="input flex-1"></input>
+                    <input ref={panRef} type="password" placeholder="PAN number" className="input flex-1"></input>
+                    <button className="button w-fit"onClick={
+                        () => {
+                            updateState(amountRef.current.value, fund_id)
+                            amountRef.current.value="";
+                            panRef.current.value=""
+                        }}> 
+                        <a> Donate </a>
+                    </button>
                 </div>
                 
                 <span className="m-2 text-xl text-center font-medium"> Previous Donations </span>
@@ -29,6 +42,31 @@ export default function FundDetails({ fund }) {
             </div>
         </FullpageCard>
     )          
+}
+
+async function updateState(amount, fund_id){
+    const body = JSON.stringify({
+        amount: parseInt(amount)
+    })
+    console.log(body)
+    try{
+        console.log(fund_id)
+        const res = await fetch('http://127.0.0.1:5000/donations/new?' + new URLSearchParams({
+            fund_id: fund_id
+        }),{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: body})
+        // console.log(res.url)
+        const {state} = await res.json()
+        // console.log(state)
+        Router.reload(window.location.pathname)
+    }catch(e){
+        console.log(e)
+    }
 }
 
 export async function getStaticProps(context) {
@@ -42,7 +80,8 @@ export async function getStaticProps(context) {
 
     return {
       props: {
-        fund
+        fund,
+        fund_id: id
       },
     }
 }
@@ -59,6 +98,7 @@ export async function getStaticPaths() {
       fallback: false
     };
   }
+
 const back_svg = <svg className="m-1" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711
                     7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import { StateProvider } from "../components/appcontext";
 import FundCard from "../components/fund_card";
 import NewFundFormCard from "../components/newfundform_card";
 import TitleCard from "../components/title_card";
 
 export default function Home({ state, funds, fund_id_list }) {
     return (
-        <StateProvider state={state}>
+        <>
             <TitleCard />
             <NewFundFormCard />
             <div className="grid grid-cols-1 lg:grid-cols-2">
                 { fund_id_list.map((fund_id) => <FundCard fund={funds[fund_id]} fund_id={fund_id}/> ) }
             </div>
-        </StateProvider>
+        </>
     )          
 }
 
