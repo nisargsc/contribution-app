a406255ef3a157121e2aa8840e3b60e126cdbb47
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-04-26 18:25:03 +0530

    add_fund and add_donation forms working

--- components/newfundform_card.js
+++ components/newfundform_card.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import { useChainStateUpdate } from './appcontext';
+import Router from 'next/router'
+
+const NewFundFormCard = () => {
+    
+    let titleRef = React.createRef();
+    let descRef = React.createRef();
+    let panRef = React.createRef();
+
+    return (
+            <div className="card flex flex-col p-2 items-center">
+                <span className="m-2 text-center">Information in blockchain is immutable so please verify before submiting</span>
+                <input ref={titleRef} type="text" placeholder="Title" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2"></input>
+                <textarea ref={descRef} type="text" placeholder="Description" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2 min-h-[3.5rem] resize-none focus:resize-y overflow-y-auto"></textarea>
+                <input ref={panRef} type="password" placeholder="PAN number" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2"></input>
+                <div className="flex justify-center">
+                    <button className="button font-medium" onClick={
+                        () => {
+                            updateState(titleRef.current.value, descRef.current.value)
+                            titleRef.current.value="";
+                            descRef.current.value="";
+                            panRef.current.value=""
+                        }}> 
+                        <a> Create New Fund </a>
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
 async function updateState(title, description){
     const body = JSON.stringify({
@@ -15,42 +42,13 @@ async function updateState(title, description){
             'Content-Type': 'application/json',
         },
         body: body})
-        console.log(res.status)
+        // console.log(res.status)
         const {state} = await res.json()
         // console.log(state)
-        return state
+        Router.reload(window.location.pathname)
     }catch(e){
         console.log(e)
     }
 }
 
-const NewFundFormCard = () => {
-    const setChainState = useChainStateUpdate()
-    
-    let titleRef = React.createRef();
-    let descRef = React.createRef();
-    let panRef = React.createRef();
-
-    return (
-            <div className="card flex flex-col p-2 items-center">
-                <span className="m-2 text-center">Information in blockchain is immutable so please verify before submiting</span>
-                <input ref={titleRef} type="text" placeholder="Title" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2"></input>
-                <textarea ref={descRef} type="text" placeholder="Description" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2 min-h-[3.5rem] resize-none focus:resize-y overflow-y-auto"></textarea>
-                <input ref={panRef} type="password" placeholder="PAN number" className="input w-full sm:w-3/4 md:w-2/3 lg:w-1/2"></input>
-                <div className="flex justify-center">
-                    <button className="button font-medium" onClick={
-                        () => {
-                            // setChainState({})
-                            setChainState(updateState(titleRef.current.value, descRef.current.value))
-                            titleRef.current.value="";
-                            descRef.current.value="";
-                            panRef.current.value=""
-                        }}> 
-                        <a> Create New Fund </a>
-                    </button>
-                </div>
-            </div>
-        )
-    }
-    
 export default NewFundFormCard;
